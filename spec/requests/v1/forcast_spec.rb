7bcd7a183231668ca54bc21de9d72fb2c11d9ac4
awosborn <alexander.w.osborne@gmail.com>
2021-03-07 15:17:31 -0700

    Build out happy path test

--- spec/requests/v1/forcast_spec.rb
+++ spec/requests/v1/forcast_spec.rb
@@ -6,6 +6,13 @@ RSpec.describe 'it returns a forecast'do
     expect(response).to be_successful
     
     forecast = JSON.parse(response.body, symbolize_names: true)
-    require 'pry'; binding.pry
+    expect(forecast).to be_a(Hash)
+    expect(forecast[:data][:attributes].keys).to match_array(%i[current_weather daily_weather hourly_weather])
+    expect(forecast[:data][:attributes][:current_weather].keys).to match_array([:conditions, :datetime, :feels_like, :humidity, :icon, :sunrise, :sunset, :temp, :uvi, :visibility])
+    expect(forecast[:data][:attributes][:daily_weather]).to be_a(Array)
+    expect(forecast[:data][:attributes][:daily_weather][0].keys).to match_array([:datetime, :sunrise, :sunset, :max_temp, :min_temp, :conditions, :icon])
+    expect(forecast[:data][:attributes][:hourly_weather]).to be_a(Array)
+    expect(forecast[:data][:attributes][:hourly_weather][0].keys).to match_array([:time, :temperature, :conditions, :icon])
+    # require 'pry'; binding.pry
   end
 end
