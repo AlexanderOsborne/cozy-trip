492c85b0706c95acbae203174392a3a384d9075c
awosborn <alexander.w.osborne@gmail.com>
2021-03-08 13:57:51 -0700

    refactor mapquest service

--- app/services/mapquest_service.rb
+++ app/services/mapquest_service.rb
@@ -2,7 +2,7 @@ class MapquestService
   class << self
     
     def coordinates(location)
-      response = conn.get do |req|
+      response = conn.get('/geocoding/v1/address') do |req|
         req.params['location'] = location
       end
       parsed = parser(response)
@@ -10,7 +10,7 @@ class MapquestService
 
   private
   def conn
-    @@conn ||= Faraday.new(:url => 'http://www.mapquestapi.com/geocoding/v1/address') do |req|
+    @@conn ||= Faraday.new(:url => 'http://www.mapquestapi.com') do |req|
       req.params['key'] = "#{ENV['GEOCODING_KEY']}"
     end
   end
