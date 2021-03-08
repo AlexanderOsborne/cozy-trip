d91f80cdd0b735c226cb021371b496906f27f06c
awosborn <alexander.w.osborne@gmail.com>
2021-03-08 14:55:21 -0700

    Add users table with unique token

--- spec/requests/v1/sessions_spec.rb
+++ spec/requests/v1/sessions_spec.rb
@@ -1,16 +1,22 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe 'it returns an image' do
-#   it 'returns JSON' do
-#     get '/api/v1/sessions'
+RSpec.describe 'it returns an image' do
+  it 'returns JSON' do
+    user = User.create(email: "admin@example.com", password: "password")
+    headers = {
+      'Accept' => 'application/json', 
+      'Content-Type' => 'application/json'
+    }
+    
+    post '/api/v1/sessions', params: JSON.generate("users": {email: "admin@example.com", password: "password"}), headers: headers
 
-#     expect(response).to be_successful
-#     expect(response.status).to eq(200)
-#   end
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+  end
 
 #   it 'Sadpaths' do
 #     get '/api/v1/backgrounds'
 
 #     expect(response.status).to eq(400)
 #   end
-# end
+end
