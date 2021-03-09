48b1e6e78861bbff5f7bf8a6b63dba9c79514f12
awosborn <alexander.w.osborne@gmail.com>
2021-03-08 17:20:30 -0700

    Add User model test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   has_secure_token :api_key
 
   validates_presence_of :email,
-                        :password_digest
+                        :password_digest,
+                        :api_key
 end
