7e94cb5b0b37acb5e0adf5c9ade454a1d1e27227
awosborn <alexander.w.osborne@gmail.com>
2021-03-09 15:02:05 -0700

    Short trip happy path working

--- app/poros/roadtrip.rb
+++ app/poros/roadtrip.rb
@@ -9,7 +9,7 @@ class Roadtrip
     @id = nil
     @start_city = data[:start_city]
     @end_city = data[:end_city]
-    @travel_time = data[:travel_time]
+    @travel_time = data[:travel_time].travel_time
     @weather_at_eta = data[:weather_at_eta]
   end
 end
--- spec/requests/v1/road_trip_spec.rb
+++ spec/requests/v1/road_trip_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'it creates a road_trip' do
-  it 'Happy path' do
+  it 'Happy path, short trip' do
     headers = {
       'Accept' => 'application/json', 
       'Content-Type' => 'application/json'
@@ -13,17 +13,52 @@ RSpec.describe 'it creates a road_trip' do
       "api_key": "jgn983hy48thw9begh98h4539h4"
     }), headers: headers
 
-    
+    expect(response).to be_successful
+    expect(response.status).to eq(201)
+
+    roadtrip = JSON.parse(response.body, symbolize_names: true)
+    expect(roadtrip).to be_a(Hash)
+    expect(roadtrip[:data][:id]).to eq(nil)
+    expect(roadtrip[:data][:type]).to eq("roadtrip")
+    expect(roadtrip[:data][:attributes].keys).to match_array([:start_city, :end_city, :travel_time, :weather_at_eta])
+    expect(roadtrip[:data][:attributes][:start_city]).to be_a(String)
+    expect(roadtrip[:data][:attributes][:end_city]).to be_a(String)
+    expect(roadtrip[:data][:attributes][:travel_time]).to be_a(String)
+    expect(roadtrip[:data][:attributes][:weather_at_eta][:conditions]).to be_a(String)
+    expect(roadtrip[:data][:attributes][:weather_at_eta][:temperature]).to be_a(Float)
+  end
+
+  it 'Happy path, long trip' do
+    headers = {
+      'Accept' => 'application/json', 
+      'Content-Type' => 'application/json'
+    }
+
+    post '/api/v1/roadtrip', params: JSON.generate({
+      "origin": "New York,NY",
+      "destination": "Los Angles, CA",
+      "api_key": "jgn983hy48thw9begh98h4539h4"
+    }), headers: headers
 
     expect(response).to be_successful
     expect(response.status).to eq(201)
-    require 'pry'; binding.pry
-    
-  end
 
+    require 'pry'; binding.pry
+    roadtrip = JSON.parse(response.body, symbolize_names: true)
+    expect(roadtrip).to be_a(Hash)
+    expect(roadtrip[:data][:id]).to eq(nil)
+    expect(roadtrip[:data][:type]).to eq("roadtrip")
+    expect(roadtrip[:data][:attributes].keys).to match_array([:start_city, :end_city, :travel_time, :weather_at_eta])
+    expect(roadtrip[:data][:attributes][:start_city]).to be_a(String)
+    expect(roadtrip[:data][:attributes][:end_city]).to be_a(String)
+    expect(roadtrip[:data][:attributes][:travel_time]).to be_a(String)
+    expect(roadtrip[:data][:attributes][:weather_at_eta][:conditions]).to be_a(String)
+    expect(roadtrip[:data][:attributes][:weather_at_eta][:temperature]).to be_a(Float)
+  end
+  
   it 'Sadpaths' do
     post '/api/v1/roadtrip'
 
-    expect(response.status).to eq(400)
+    expect(response.status).to eq(401)
   end
 end
